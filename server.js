18ec551a9af11b38eed3afe16fdea326629c79e3
debilla <dimuthu.billa@gmail.com>
2023-09-14 11:56:48 +0800

    edits

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3002;
 
-app.use(express.static('outputdash'));
+app.use(express.static('output_dash'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html')); // Replace with your HTML file
